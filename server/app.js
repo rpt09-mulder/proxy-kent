598f0af588ff747f30b4c3f83ff32dc17aa00371
Kent Ogisu <kentogisu@gmail.com>
2018-12-22 14:32:54 -0500

    [Refactor] Added script tags for services

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 //Middleware
 app.use(bodyParser.json());
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+const app = require('./app.js');
 
 //setup env variavles
 require('dotenv').config();
